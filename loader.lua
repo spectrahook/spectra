de4c57ef4f032d477f34aa7dcdd1cd20e3648a80
spectrahook <kosim4620@gmail.com>
2025-06-25 00:42:15 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,3 @@
-
 local id = game.PlaceId
 if id == 126244816328678 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/126244816328678.lua", true))()
