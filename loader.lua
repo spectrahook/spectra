e2e609fc18121b791aa61e5e0ea1131150406d3d
spectrahook <kosim4620@gmail.com>
2025-06-25 00:40:19 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,7 @@
-local game = {
-    [126244816328678] = true,
-    [116793284465110] = true
-}
 
 local id = game.PlaceId
-if game[id] then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/" .. id .. ".lua", true))()
+if id == 126244816328678 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/126244816328678.lua", true))()
+elseif id == 116793284465110 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/116793284465110.lua", true))()
 end
