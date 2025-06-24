60e592f82e4667251fa3d139af9e3f470bcf93ea
spectrahook <kosim4620@gmail.com>
2025-06-25 00:50:54 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,13 @@
-local id = game.PlaceId
-if id == 126244816328678 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/126244816328678.lua", true))()
-elseif id == 116793284465110 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/116793284465110.lua", true))()
+
+local games = {
+    [126244816328678] = "https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/126244816328678.lua",
+    [116793284465110] = "https://raw.githubusercontent.com/spectrahook/spectra/refs/heads/main/Games/116793284465110.lua"
+}
+
+local place_id = game.PlaceId
+
+if games[place_id] then
+    loadstring(game:HttpGet(games[place_id]))()
+else
+    print("not supported vro : " .. place_id)
 end
